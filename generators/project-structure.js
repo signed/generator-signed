0a209a37473d932026337b9c9566049a1b3ea21a
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2016-06-28 22:47:06 +0200

    decision if the project root is a sub directory is in one place

--- generators/project-structure.js
+++ generators/project-structure.js
@@ -9,29 +9,36 @@ function ProjectStructure(generator) {
 ProjectStructure.prototype.scaffoldInProjectRoot = function (path) {
   this.generator.fs.copy(
     this.generator.templatePath(path),
-    this.generator.props.projectName + '/' + path
+    this._projectRoot(path)
   );
 };
 
 ProjectStructure.prototype.scaffoldGlobIn = function (glob, destination, options) {
   this.generator.fs.copy(
     this.generator.templatePath(glob),
-    this.generator.props.projectName + '/' + destination,
-    options);
+    this._projectRoot(destination),
+    options
+  );
 };
 
 ProjectStructure.prototype.scaffoldTemplateInProjectRoot = function (path, templateVariables) {
   this.generator.fs.copyTpl(
     this.generator.templatePath(path),
-    this.generator.props.projectName + '/' + path, templateVariables);
+    this._projectRoot(path),
+    templateVariables
+  );
 };
 
 ProjectStructure.prototype.scaffoldJavaFile = function (javaFile) {
   var destinationPath = javaFile.replace('package', this.generator.props.package.split('.').join('/'));
   this.generator.fs.copyTpl(
     this.generator.templatePath(javaFile),
-    this.generator.destinationPath(this.generator.props.projectName + '/' + destinationPath), {
+    this._projectRoot(destinationPath), {
       package: this.generator.props.package
     }
   );
+};
+
+ProjectStructure.prototype._projectRoot = function (path) {
+  return this.generator.destinationPath(this.generator.props.projectName + '/' + path);
 };
